5abbb4837a10a44007c52721155928875e1089db
leebohyeon1 <debug.bohyeon@gmail.com>
2025-08-22 22:15:50 +0900

    Feat: 플레이어 등록

--- Assets/Prototype/02_Feature/02_Character/01_Player/Scripts/PlayerController.cs
+++ Assets/Prototype/02_Feature/02_Character/01_Player/Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
 using System.Threading.Tasks;
 using BH_Lib.AssetManager;
+using BH_Lib.DI;
 using UnityEngine;
 
 /// <summary>
 /// 플레이어의 입력을 받아 캐릭터를 제어하는 컨트롤러 (InputSystem 사용)
 /// AssetManager를 통해 InputReader를 비동기로 로딩
 /// </summary>
-[RequireComponent(typeof(IMovable), typeof(IAttacker))]
+
+[Register(typeof(PlayerController),LifetimeScope.Transient)]
 public class PlayerController : MonoBehaviour
 {
     [Header("Dependencies")]
