272ad2d929a35edba7161aedd3849ccb5562a1d7
leebohyeon1 <debug.bohyeon@gmail.com>
2025-08-23 16:18:16 +0900

    Refactor: 인터페이스 접근제한자 명시
    인터페이스는 기본 public이지만 그래도 public 접근제한자를 명시함

--- Assets/_GraduationProject/02_Feature/01_Core/01_Interface/IHealable.cs
+++ Assets/_GraduationProject/02_Feature/01_Core/01_Interface/IHealable.cs
@@ -7,5 +7,5 @@ public interface IHealable
     /// 대상의 체력을 회복시킵니다.
     /// </summary>
     /// <param name="healAmount">회복시키고자 하는 양입니다.</param>
-    void Heal(float healAmount);
+    public void Heal(float healAmount);
 }
--- Assets/_GraduationProject/02_Feature/01_Core/01_Interface/IMovable.cs
+++ Assets/_GraduationProject/02_Feature/01_Core/01_Interface/IMovable.cs
@@ -5,11 +5,11 @@ public interface IMovable
     /// <summary>
     /// 현재 이동 속도
     /// </summary>
-    float MoveSpeed { get; }
+    public float MoveSpeed { get; }
 
     /// <summary>
     /// 이동 방향으로 움직이는 함수
     /// </summary>
     /// <param name="direction">이동할 방향</param>
-    void Move(Vector3 direction);
+    public void Move(Vector3 direction);
 }
