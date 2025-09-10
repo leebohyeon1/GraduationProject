df4b9cafb43a33740be65ba44c3bb62c94fdaed7
leebohyeon1 <debug.bohyeon@gmail.com>
2025-09-10 15:25:57 +0900

    Feat: 테스트 적에 공격 추가 및 IParryable 수정
    IParryable에 카운터 기능 제거
    카운터가 특정 대상에게 하는 것이 아닌듯

--- Assets/_GraduationProject/02_Feature/01_Core/01_Interface/IParryable.cs
+++ Assets/_GraduationProject/02_Feature/01_Core/01_Interface/IParryable.cs
@@ -9,21 +9,11 @@ public interface IParryable
     /// 현재 패링이 가능한 상태인지 여부를 나타냅니다.
     /// </summary>
     public bool IsParryable { get; }
-
-    /// <summary>
-    /// 카운터 공격 가능 여부를 나타냅니다.
-    /// </summary>
-    public bool CanCounterAttack { get; }
-
+    
     /// <summary>
     /// 이 오브젝트에 대한 패링을 시도합니다.
     /// </summary>
     /// <param name="parryInstigator">패링을 시도한 주체 (예: 플레이어)의 GameObject입니다.</param>
     /// <returns>패링 성공 여부를 반환합니다.</returns>
     public bool Parry(GameObject parryInstigator);
-
-    /// <summary>
-    /// 카운터 공격을 시도합니다.
-    /// </summary>
-    public void CounterAttack();
 }
