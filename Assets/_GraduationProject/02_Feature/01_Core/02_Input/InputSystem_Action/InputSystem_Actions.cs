690cb06ae5117023b0299e970e42cda93e3d3704
leebohyeon1 <debug.bohyeon@gmail.com>
2025-09-10 20:03:57 +0900

    Assets: 플레이어 애니메이션 수정

--- Assets/_GraduationProject/02_Feature/01_Core/02_Input/InputSystem_Action/InputSystem_Actions.cs
+++ Assets/_GraduationProject/02_Feature/01_Core/02_Input/InputSystem_Action/InputSystem_Actions.cs
@@ -312,7 +312,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""1c04ea5f-b012-41d1-a6f7-02e963b52893"",
-                    ""path"": ""<Keyboard>/e"",
+                    ""path"": ""<Keyboard>/f"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""Keyboard&Mouse"",
@@ -345,7 +345,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""a2fc94c7-8d4a-4256-ba7e-53e015343af1"",
-                    ""path"": ""<Keyboard>/tab"",
+                    ""path"": ""<Keyboard>/e"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": "";Keyboard&Mouse"",
