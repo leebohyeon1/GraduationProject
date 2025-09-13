afd1e848dcfe1ab15ec3cf01649374578ead573c
leebohyeon1 <debug.bohyeon@gmail.com>
2025-09-14 00:06:35 +0900

    Refactor: 이벤트 데이터는 따로 스크립트 분리

--- Assets/_GraduationProject/02_Feature/02_Character/01_Player/Scripts/PlayerEventChannel.cs
+++ Assets/_GraduationProject/02_Feature/02_Character/01_Player/Scripts/PlayerEventChannel.cs
@@ -80,41 +80,8 @@ public class PlayerEventChannel
         ChargeMeleeAttack.Dispose();
         CounterAttack.Dispose();
     }
-    
+
     #endregion
-}
-
-
-/// <summary>
-/// 체력 변경 이벤트에 사용되는 데이터 구조체입니다.
-/// </summary>
-[Serializable]
-public struct HealthChangeEventData
-{
-    /// <summary>
-    /// 이전 체력
-    /// </summary>
-    public int PreviousHealth { get; set; }
-    /// <summary>
-    /// 현재 체력
-    /// </summary>
-    public int CurrentHealth { get; set; }
-    /// <summary>
-    /// 최대 체력
-    /// </summary>
-    public int MaxHealth { get; set; }
-    /// <summary>
-    /// 데미지를 입었는지 여부
-    /// </summary>
-    public bool IsDamage => CurrentHealth < PreviousHealth;
-    /// <summary>
-    /// 치유되었는지 여부
-    /// </summary>
-    public bool IsHeal => CurrentHealth > PreviousHealth;
-    /// <summary>
-    /// 현재 체력 비율
-    /// </summary>
-    public float HealthPercent => (float)CurrentHealth / MaxHealth;
 }
 
 /// <summary>
