0f70b0af70bfbde33e7352969ee4471558447519
KangUBIN <yubin3253@naver.com>
2025-10-04 00:40:09 +0900

    잔디쉐이더,시스루
    잔디 수정 및 추가했습니다

---
+++ Packages/com.shadercrew.seethroughshader.core/Scripts/Editor/Common/AbsoluteValueDrawer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ShaderCrew.SeeThroughShader
+{
+    public class AbsoluteValueDrawer : MaterialPropertyDrawer
+    {
+
+        public AbsoluteValueDrawer()
+        {
+        }
+
+        public override void OnGUI(Rect position, MaterialProperty property, string label, MaterialEditor editor)
+        {
+            if (property.propertyType == UnityEngine.Rendering.ShaderPropertyType.Float)
+            {
+                property.floatValue = Mathf.Max(EditorGUI.FloatField(position, label, property.floatValue), 0);
+            }
+            else
+            {
+                editor.DefaultShaderProperty(property, label);
+            }
+        }
+
+    }
+}
