70a91b63ed59367f2284010bbf04536536e2bdee
gmy0150 <gmy0150@naver.com>
2025-10-20 19:21:18 +0900

    stun animation

--- Assets/_GraduationProject/02_Feature/02_Character/02_NPC/01_Enemy/05_Scripts/ParrySystem.cs
+++ Assets/_GraduationProject/02_Feature/02_Character/02_NPC/01_Enemy/05_Scripts/ParrySystem.cs
@@ -8,16 +8,16 @@ public class ParrySystem : MonoBehaviour, IParryable, ICounterable
     public bool IsParryable { get; private set; } = false;
     public bool IsCounterable { get; private set; } = false;
     float _stunExitTime = -Mathf.Infinity;
-    public bool _isStunned { get;  private set; } = false;
+    public bool _isStunned { get; private set; } = false;
     public float StunExitTime => _stunExitTime;
     [SerializeField] private float _stunTime = 3f;
     private Enemy _owner;
     public void Initialize(Enemy enemy)
     {
         _owner = enemy;
-        ClearStun();
         IsParryable = false;
         IsCounterable = false;
+        ClearStun();
     }
     public void SetParryable(string value)
     {
@@ -42,12 +42,13 @@ public class ParrySystem : MonoBehaviour, IParryable, ICounterable
         return true;
     }
 
-        public void ApplyStun()
+    public void ApplyStun()
     {
         if (_isStunned || _owner.EnemyHealth.IsDead) return; // 이미 스턴 상태라면 무시
         _isStunned = true;
         _stunExitTime = Time.time + _stunTime;
         _owner.Movement.StopMovement(); // 스턴 상태에서는 이동을 멈춥니다.
+        Debug.Log("ApplyStun ");
         _owner.animator.SetBool("Stun", true); // 스턴 애니메이션 트리거
     }
     public void ApplyStun(float stunDuration)
@@ -56,6 +57,7 @@ public class ParrySystem : MonoBehaviour, IParryable, ICounterable
         _isStunned = true;
         _stunExitTime = Time.time + stunDuration;
         _owner.Movement.StopMovement(); // 스턴 상태에서는 이동을 멈춥니다.
+        Debug.Log("dsd");
         _owner.animator.SetBool("Stun", true); // 스턴 애니메이션 트리거
     }
 
