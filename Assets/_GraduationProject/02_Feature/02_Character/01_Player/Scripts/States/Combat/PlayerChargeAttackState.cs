6fcd4e65edce18a1a3b976f14ed53a2944069d30
leebohyeon1 <debug.bohyeon@gmail.com>
2025-10-30 01:41:17 +0900

    Fix: 차징 공격 시 열기가 떨어지지 않는 버그 수정

--- Assets/_GraduationProject/02_Feature/02_Character/01_Player/Scripts/States/Combat/PlayerChargeAttackState.cs
+++ Assets/_GraduationProject/02_Feature/02_Character/01_Player/Scripts/States/Combat/PlayerChargeAttackState.cs
@@ -91,8 +91,9 @@ public class PlayerChargeAttackState : PlayerAttackBaseState
             p_context.Events.TriggerChargeAttackAffect(collider);
         }
 
-        p_context.Heat.TriggerChargeGuageChanged(0f);
         p_context.Heat.ChangeHeat(-Mathf.FloorToInt(p_context.Heat.CurrentChargeGuage));
+        p_context.Heat.TriggerChargeGuageChanged(0f);
+
     }
 
     /// <summary>
