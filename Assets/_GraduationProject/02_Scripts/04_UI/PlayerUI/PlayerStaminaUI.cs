6e643fb618fc440cfe30ad6646735c2e3a07d1d7
LEEBOHYEON <42289387+leebohyeon1@users.noreply.github.com>
2025-12-01 22:40:11 +0900

    Player dummy UI (#138)
    * Feat: 체력, 스테미나 UI 기능
    
    * 원형 UI 플레이어 추적
    
    * 따라다니는 코드 조금 수정
    
    * HUD 프리팹화

--- Assets/_GraduationProject/02_Scripts/04_UI/PlayerUI/PlayerStaminaUI.cs
+++ Assets/_GraduationProject/02_Scripts/04_UI/PlayerUI/PlayerStaminaUI.cs
@@ -1,15 +1,29 @@
 using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerStaminaUI : MonoBehaviour
 {
     [SerializeField] private PlayerStamina _playerStamina;
-    [SerializeField] private Image _staminaImage;
+    [SerializeField] private Image _plusStaminaImage;
+    [SerializeField] private Image _minusStaminaImage;
+
+    [Tooltip("게이지가 차오르거나 줄어드는 속도 (초당 퍼센트)")]
+    [SerializeField] private float _fillSpeed = 2.0f; // 1초에 2칸(200%) 정도 이동하는 속도
+
+    private float _currentAmount = 0;
 
     private void OnEnable()
     {
+        // 초기화: UI가 켜질 때 현재 스테미나로 즉시 설정
         _playerStamina.OnStaminaChanged += HandleStaminaChanged;
+    }
+
+    private void Start()
+    {
+        _currentAmount = _playerStamina.Stamina;
+        UpdateImages(_playerStamina.Stamina);
     }
 
     private void OnDisable()
@@ -17,20 +31,39 @@ public class PlayerStaminaUI : MonoBehaviour
         _playerStamina.OnStaminaChanged -= HandleStaminaChanged;
     }
 
-
     private void HandleStaminaChanged(float previousStamina, float currentStamina)
     {
         DOTween.Kill(this);
 
         DOTween.To(
-            () => _staminaImage.fillAmount,
+            () => _currentAmount,
             X =>
             {
-                _staminaImage.fillAmount = X;
+                _currentAmount = X;
+                UpdateImages(_currentAmount);
             },
-            currentStamina / _playerStamina.MaxStamina,
-            0.3f)
+           currentStamina,
+             _fillSpeed)
             .SetEase(Ease.Linear)
             .SetId(this);
-    }    
+    }
+
+    // 실제 이미지를 채우는 로직 분리
+    private void UpdateImages(float staminaValue)
+    {
+        float maxStamina = _playerStamina.MaxStamina;
+
+        if (staminaValue >= 0)
+        {
+            // 양수 구간: Plus는 비율대로, Minus는 0
+            _plusStaminaImage.fillAmount = staminaValue / maxStamina;
+            _minusStaminaImage.fillAmount = 0f;
+        }
+        else
+        {
+            // 음수 구간: Plus는 0, Minus는 절대값 비율대로
+            _plusStaminaImage.fillAmount = 0f;
+            _minusStaminaImage.fillAmount = Mathf.Abs(staminaValue) / maxStamina;
+        }
+    }
 }
