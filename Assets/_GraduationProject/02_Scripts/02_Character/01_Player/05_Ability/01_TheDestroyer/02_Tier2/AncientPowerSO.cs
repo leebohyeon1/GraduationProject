83e9b9df040a82c3973effd0fcaa80c5d3c93859
leebohyeon1 <debug.bohyeon@gmail.com>
2026-02-04 10:57:04 +0900

    AncientPowerSO 해제 수정

--- Assets/_GraduationProject/02_Scripts/02_Character/01_Player/05_Ability/01_TheDestroyer/02_Tier2/AncientPowerSO.cs
+++ Assets/_GraduationProject/02_Scripts/02_Character/01_Player/05_Ability/01_TheDestroyer/02_Tier2/AncientPowerSO.cs
@@ -13,8 +13,6 @@ public class AncientPowerSO : PlayerAbilitySO
     public List<PlayerChargeConfig> AdditionalChargeDataList = new List<PlayerChargeConfig>();
     public float ChargeMoveSpeed; // 차지 이동속도
 
-    private int _defaultIndex = -1;
-
     /// <summary>
     /// 기능 등록
     /// </summary>
@@ -23,8 +21,6 @@ public class AncientPowerSO : PlayerAbilitySO
     {
         p_ability = ability;
         p_owner = p_ability.GetComponent<PlayerController>();
-
-        _defaultIndex = p_owner.Combat.HeavyCounterAttackConfigList.Count;
 
         p_owner.Combat.HeavyCounterAttackConfigList.AddRange(AdditionalChargeDataList);
         p_owner.Movement.SetChargeMoveSpeed(ChargeMoveSpeed);
@@ -36,15 +32,12 @@ public class AncientPowerSO : PlayerAbilitySO
     /// <param name="ability">플레이어</param>
     public override void UnregisterAbility(PlayerAbility ability)
     {
-        if (p_owner != null)
+        foreach (var data in AdditionalChargeDataList)
         {
-            p_owner.Combat.HeavyCounterAttackConfigList
-                .RemoveRange(_defaultIndex, AdditionalChargeDataList.Count);
+            p_owner.Combat.HeavyCounterAttackConfigList.Remove(data);
         }
 
         p_ability = null;
         p_owner = null;
-
-        _defaultIndex = -1;
     }
 }
