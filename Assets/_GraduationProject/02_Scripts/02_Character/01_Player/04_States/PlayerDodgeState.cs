97da36d33ac07843f51e955381f9edca43055a43
leebohyeon1 <debug.bohyeon@gmail.com>
2026-04-16 00:03:28 +0900

    회피 스테미나 사용

--- Assets/_GraduationProject/02_Scripts/02_Character/01_Player/04_States/PlayerDodgeState.cs
+++ Assets/_GraduationProject/02_Scripts/02_Character/01_Player/04_States/PlayerDodgeState.cs
@@ -52,6 +52,8 @@ public class PlayerDodgeState : PlayerBaseState
     {
         base.SetupStats();
 
+        // 스테미나 소모 (필요 시 데이터에서 가져오도록 수정 가능)
+        p_owner.Stamina.UseStamina(p_owner.Movement.DodgeConfig.StaminaConsumption.Value);
         p_owner.Combat.ResetNormalAttackComboIndex();       // 일반 공격 콤보 순서 초기화
     }
 
