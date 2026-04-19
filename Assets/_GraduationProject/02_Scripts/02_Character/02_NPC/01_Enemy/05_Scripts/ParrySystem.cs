73f688d6106f43c09f0b5d2b4d45f721d12b3abd
leebohyeon1 <debug.bohyeon@gmail.com>
2026-04-19 15:59:50 +0900

    패리

--- Assets/_GraduationProject/02_Scripts/02_Character/02_NPC/01_Enemy/05_Scripts/ParrySystem.cs
+++ Assets/_GraduationProject/02_Scripts/02_Character/02_NPC/01_Enemy/05_Scripts/ParrySystem.cs
@@ -65,13 +65,11 @@ public class ParrySystem : MonoBehaviour, IParryable, ICounterable
         if(attackType == AttackType.Normal_Counter)
         {
             Debug.Log("[ParrySystem] 카운터 공격이 성공했습니다!");
-            _owner.StiffnessSystem.AddStiffness(_owner.player.Data.NormalAttackConfigList[0].Stiffness, true);
             DeactivateImmunity();
         }
         else
         {
             Debug.Log("[ParrySystem] 경직이 적용되었습니다!");
-            _owner.StiffnessSystem.AddStiffness(_owner.player.Data.HeavyAttackConfigList[0].Stiffness, true);
             DeactivateImmunity();
         }
         return true;
