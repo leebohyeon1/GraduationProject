ee0e55ea8972f97874ec25a67cf989c3436eb7e0
leebohyeon1 <debug.bohyeon@gmail.com>
2026-04-22 21:21:42 +0900

    오쁘닝 컷씬

--- Assets/_GraduationProject/02_Scripts/02_Character/01_Player/04_States/Combat/PlayerNormalCounterState.cs
+++ Assets/_GraduationProject/02_Scripts/02_Character/01_Player/04_States/Combat/PlayerNormalCounterState.cs
@@ -121,7 +121,7 @@ public class PlayerNormalCounterState : PlayerAttackBaseState
         if (transform.TryGetComponent<IStiffness>(out var stiffness))
         {
             int counterStiffness = (int)p_AttackConfig.Stiffness.Value - baseStiffness; // 카운터로 인한 추가 경직량 계산
-            stiffness.AddStiffness(counterStiffness, p_AttackConfig.AttackType);
+            stiffness.AddStiffness(counterStiffness, AttackType.Normal_Counter);
             p_AttackConfig.Stiffness.RemoveAllModifiersFromSource("NormalCounterStiffness");
         }
 
