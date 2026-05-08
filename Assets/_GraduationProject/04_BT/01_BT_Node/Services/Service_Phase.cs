6c0baf1cfcadeaf1487aa6f43ae64df43b723984
gmy0150 <gmy0150@naver.com>
2026-05-09 00:41:45 +0900

    service float으로 변경

--- Assets/_GraduationProject/04_BT/01_BT_Node/Services/Service_Phase.cs
+++ Assets/_GraduationProject/04_BT/01_BT_Node/Services/Service_Phase.cs
@@ -15,18 +15,20 @@ public class Service_Phase : ServiceNode
             return;
         }
         float healthPercent = (float)runner.EnemyHealth.CurrentHealth / (float)runner.EnemyHealth.MaxHealth;
-        int phase = 1;
+        float phase = 1.0f;
 
         if (healthPercent <= 0.5f)
         {
             Debug.Log($"healthPercent: {healthPercent} runner.EnemyHealth.CurrentHealth: {runner.EnemyHealth.CurrentHealth} runner.EnemyHealth.MaxHealth: {runner.EnemyHealth.MaxHealth}    ");   
-            phase = 2;
+            phase = 2.0f;
         }
         // if (healthPercent <= 0.25f)
         // {
         //     phase = 2;
         // }
-        if(runner._aiController._aiBrain.blackboard.GetValue<int>(EnemyBlackboardKeys.Phase) != phase)
+        if(runner._aiController._aiBrain.blackboard.GetValue<float>(EnemyBlackboardKeys.Phase) != phase)
             runner._aiController._aiBrain.blackboard.SetValue(EnemyBlackboardKeys.Phase, phase);
+
+        Debug.Log($"[Service_Phase] Health Percent: {healthPercent}, Current Phase: {runner._aiController._aiBrain.blackboard.GetValue<float>(EnemyBlackboardKeys.Phase)}");
     }
 }
