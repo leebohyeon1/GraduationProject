42d3556c71baa899a0c6e0578220a77d433711bc
hdj5069 <gusehdwn763@gmail.com>
2026-05-09 00:09:26 +0900

    보스몬스터 조정중

--- Assets/_GraduationProject/04_BT/01_BT_Node/Decorator/Condition_InteractEnemy.cs
+++ Assets/_GraduationProject/04_BT/01_BT_Node/Decorator/Condition_InteractEnemy.cs
@@ -9,12 +9,12 @@ public class Condition_InteractEnemy : ConditionNode
     {
         base.initNode();
         interactComponent = runner?.GetComponent<EnemyInteract>();
-        Debug.Log($"[Condition_InteractEnemy] Initialized with EnemyInteract component: {interactComponent != null}");
+        //Debug.Log($"[Condition_InteractEnemy] Initialized with EnemyInteract component: {interactComponent != null}");
     }
     protected override bool CheckCondition()
     {
         bool isInteracted = interactComponent != null && interactComponent._isInteracted;   
-        Debug.Log($"[Condition_InteractEnemy] Checking interaction condition: Is Interacted: {isInteracted}");
+        //Debug.Log($"[Condition_InteractEnemy] Checking interaction condition: Is Interacted: {isInteracted}");
         // runner의 EnemyInteract 컴포넌트를 확인하여 상호작용 여부를 판단합니다.
         return isInteracted;
     }
